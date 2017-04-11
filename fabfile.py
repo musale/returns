9bda5d12ffe7f69c95969caaf38d0d6e75bdad1f
Eutychus Towett <ektowett@gmail.com>
2017-04-11 12:06:10 +0300

    rotate logs, fab for deploy

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,97 @@
-#!/usr/bin/env python
 
-from fabric.api import env, cd, run, sudo
+import os
+
+from fabric.api import env, cd, run, sudo, local, lcd, put
+from fabric.contrib.files import exists
+from fabric.colors import green
 
 env.use_ssh_config = True
 env.hosts = ["mpesa"]
-code_dir = "/home/ekt/go/src/github.com/etowett/returns/"
+
+local_dir = "/home/ekt/go/src/github.com/etowett/"
+live_dir = "/home/focus/go/src/github.com/etowett/"
 install_dir = "/apps/returns/"
+tmp = "/tmp/returns"
+tmp_f = "%s/returns.tar.gz" % tmp
+user = "focus"
+
+
+def stage():
+    print(green("Deploying to stage"))
+    env.hosts = ['sms']
 
 
 def deploy():
-    with cd(code_dir):
+    print(green("pulling changes"))
+    with cd("%s/returns" % (live_dir,)):
         run("git pull origin master")
+        run("go get")
         run("go build")
         run("go install")
-    sudo("systemctl stop callbacks")
+    print(green("stop service"))
+    stop_returns()
     with cd(install_dir):
         run("rm returns")
         run("cp /home/ekt/go/bin/returns .")
-    sudo("systemctl start callbacks")
+    print(green("start service"))
+    restart_returns()
+    return
+
+
+def xdeploy():
+    if os.path.exists(tmp):
+        local('rm -rf %s' % tmp)
+    local('mkdir %s' % tmp)
+    with lcd(local_dir):
+        local('tar -czhf %s returns --exclude=".git*"' % (tmp_f))
+    if exists(tmp):
+        run('rm -rf %s' % tmp)
+    run('mkdir %s' % tmp)
+    put(tmp_f, tmp_f)
+    with cd(live_dir):
+        if exists('returns'):
+            run('rm -rf returns')
+        run('tar -xzf %s' % tmp_f)
+    with cd('%s/returns' % live_dir):
+        run('go get')
+        run('go build')
+        run('go install')
+    restart_returns()
+    return
+
+
+def setup():
+    sudo("yum -y install go git")
+    if not exists("/home/focus/go"):
+        run("mkdir /home/focus/go")
+        run("echo \"export GOPATH=$HOME/go\" >> /home/focus/.bashrc")
+    run("go get github.com/etowett/returns")
+    with cd('%s/returns' % live_dir):
+        run('go get')
+        run('go build')
+        run('go install')
+    if not exists("/apps"):
+        sudo("mkdir /apps")
+        sudo("chown %s:%s /apps" % (user, user,))
+    with cd("/apps"):
+        if not exists("returns"):
+            run("mkdir returns")
+        with cd("returns"):
+            run("cp %s/returns/env.sample .env" % (live_dir,))
+            run("cp /home/focus/go/bin/returns .")
+    sudo(
+        "cp %s/returns/config/callbacks.service " +
+        "/etc/systemd/system/callbacks.service" % (live_dir,)
+    )
+    restart_returns()
+    return
+
+
+def stop_returns():
+    sudo("systemctl stop callbacks")
+    return
+
+
+def restart_returns():
+    sudo('systemctl restart returns')
     return
